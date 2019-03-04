30de73546dbeecda8d78373bdc5d6347d60b50b9
PulkitVashistha <sharmapulkit43@gmail.com>
2019-03-04 21:44:57 +0530

    cleaning

--- Collections/Q7SpecialStackImplemetation.java
+++ Collections/Q7SpecialStackImplemetation.java
@@ -1,24 +1,27 @@
 package com.ttnd.extra.Collections;
 
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 class Stack{
+
     private List<Integer> list = new LinkedList<>();
+    private TreeSet<Integer> set = new TreeSet<>();
+
     int size=10;
 
     public void push(Integer i){
-        if(!isFull())
-        list.add(i);
+        if(!isFull()) {
+            list.add(i);
+            set.add(i);
+        }
         else
             System.out.println("Stack Overflow");
     }
     public void pop(){
         if(!isEmpty()){
             System.out.println( this.list.get(list.size()-1));
-            this.list.remove(list.size()-1);
+            ;
+            this.set.remove(this.list.remove(list.size()-1));
         }
         else
             System.out.println("Stack Underflow");
@@ -29,8 +32,8 @@ class Stack{
     public boolean isFull(){
         return list.size()>=size;
     }
-    public int getMin(Stack stack){
-        return Collections.min(this.list);
+    public int getMin(){
+        return this.set.first();
     }
 
 }
@@ -57,7 +60,7 @@ public class Q7SpecialStackImplemetation {
         stack.pop();
         stack.pop();
         stack.pop();
-        System.out.println( stack.getMin(stack));
+        System.out.println( stack.getMin());
 
     }
 }
